36f69666905c0b03765db18200658103d6968d2a
salva <salroiba03@gmail.com>
2022-10-03 17:00:53 +0200

    the app works

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { ToastrModule } from 'ngx-toastr';
@@ -21,7 +20,6 @@ import { ToastrModule } from 'ngx-toastr';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    HomeModule,
     SharedModule,
     CoreModule,
     ToastrModule.forRoot({
--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListCategoriesComponent } from '../shared/list-categories/list-categories.component';
+import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from '../shared/shared.module';
-
+// import { CarouselComponent } from './carousel/carousel.component';
+// import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { ScrollComponent } from './scroll/scroll.component';
+// import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 
 @NgModule({
   declarations: [
-    ListCategoriesComponent,
-    CommonModule,
-    SharedModule,
-    HomeModule
+    HomeComponent,
+    // CarouselComponent,
+    // ScrollComponent
   ],
   imports: [
     CommonModule,
-    HomeModule
+    SharedModule,
+    HomeRoutingModule,
+    // MdbCarouselModule,
+    // NgbModule,
+    // InfiniteScrollModule
   ]
 })
 export class HomeModule { }
